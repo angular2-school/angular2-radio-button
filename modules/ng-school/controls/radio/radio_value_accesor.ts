6b881b8071690340bb519339d95982f38d74258d
01 <R3F@LW5-O7J-CEM.dsone.3ds.com>
2015-12-17 17:38:35 +0100

    radio value accessor

--- modules/ng-school/controls/radio/radio_value_accesor.ts
+++ modules/ng-school/controls/radio/radio_value_accesor.ts
@@ -7,12 +7,12 @@ import {DOM} from 'angular2/platform/common_dom';
 })
 export class RadioControlValueAccessor extends DefaultValueAccessor {
     constructor(private cd: NgControl, private renderer: Renderer, private elementRef: ElementRef) {
-        super(cd, renderer, elementRef);
+        super(renderer, elementRef);
     }
 
     writeValue(value: any) {
         if (DOM.getAttribute(this.elementRef.nativeElement, 'value') == value) {
-            DOM.setProperty(this.renderer, this.elementRef, 'checked', value);
+            DOM.setProperty(this.elementRef.nativeElement, 'checked', value);
         }
     }
 }
